93f1a9653723c6cdc9fcedf6f454b276a31e997a
Faizan <muhammadfaizanjanjua109@gmail.com>
2024-08-28 03:50:46 +0500

    addedChanges

--- src/app/page.js
+++ src/app/page.js
@@ -3,57 +3,28 @@ import styles from "./page.module.css";
 import Navbar from "@/Common/Navbar";
 import Main from "@/Common/Main";
 import Footer from "@/Common/Footer";
-
+import headerImage from '../../src/assets/images/hero-services-img.png'
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section id="hero" className="d-flex align-items-center justify-content-center">
-        <div className="container aos-init aos-animate" data-aos="fade-up">
-
-          <div className="row justify-content-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="150">
-            <div className="col-xl-6 col-lg-8">
-              <h1>Auswide Institute </h1>
-              {/* <h1>Training</h1> */}
-              <h2>Empowering Cyber Resilience Through Training</h2>
-            </div>
+      <section id="hero" className="hero section">
+      <div className="hero-bg bg-light">
+      
+      </div>
+      <div className="container text-center">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <h1 data-aos="fade-up" className="aos-init aos-animate">Welcome to <span>Auswide Training Institute</span></h1>
+          <p data-aos="fade-up" data-aos-delay="100" className="aos-init aos-animate">Specialise in providing comprehensive training programs<br/></p>
+          <div className="d-flex aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+            <a href="#about" className="btn-get-started">Get Started</a>
+            <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
-
-          <div className="row gy-4 mt-5 justify-content-center aos-init aos-animate" data-aos="zoom-in" data-aos-delay="250">
-            <div className="col-xl-2 col-md-4" href="#about">
-              <div className="icon-box">
-                <i class='bx bx-info-square'></i>
-                <h3><a className="nav-link scrollto" href="#about">About</a></h3>
-              </div>
-            </div>
-            <div className="col-xl-2 col-md-4" href="#services">
-              <div className="icon-box">
-                <i class='bx bxs-briefcase'></i>
-                <h3><a className="nav-link scrollto" href="#services">Services</a></h3>
-              </div>
-            </div>
-            <div className="col-xl-2 col-md-4" href="#resources">
-              <div className="icon-box">
-              <i class='bx bxl-sketch'></i>
-                <h3><a className="nav-link scrollto" href="#resources">Resources</a></h3>
-              </div>
-            </div>
-            <div className="col-xl-2 col-md-4" href="#cources">
-              <div className="icon-box">
-                <i class='bx bxs-book-content'></i>
-                <h3><a className="nav-link scrollto" href="#cources">Courses</a></h3>
-              </div>
-            </div>
-            <div className="col-xl-2 col-md-4" href="#contact">
-              <div className="icon-box">
-                <i class='bx bxs-contact'></i>
-                <h3><a className="nav-link scrollto" href="#contact">Contact</a></h3>
-              </div>
-            </div>
-          </div>
-
+          <Image src={headerImage} width={350} className="img-fluid  aos-init aos-animate w-50% h-50%" alt="" data-aos="zoom-out" data-aos-delay="300" />
         </div>
-      </section>
+      </div>
+
+    </section>
 
       <Main />
 
